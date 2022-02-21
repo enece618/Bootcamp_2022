a3364862931b1694207559a30621beb1230fa047
Fernando Nevarez <49252590+enece618@users.noreply.github.com>
2022-02-21 11:40:11 -0600

    primer commit

---
+++ resources/views/games/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row hidden-md-up">
+            <div class="col-md-6">
+                <h1>Videojuegos</h1>
+            </div>
+        </div>
+        <div class="py-5">
+            <div class="container">
+                <div class="row hidden-md-up">
+                    @foreach($games as $key => $game)
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="{{ $game->cover }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $game->name }}</h5>
+                                    <p class="card-text">{{ $game->description }}</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"><strong>Platform:</strong> {{$game->platform}}</li>
+                                    <li class="list-group-item"><strong>Price:</strong> ${{$game->price}}</li>
+                                </ul>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <a class="btn btn-success" href="{{ route('games.edit', $game->id) }}">
+                                                Editar
+                                            </a>
+                                        </div>
+                                        <div class="col-md-6">      
+                                            <form method="post" action="{{ route('games.destroy', $game->id) }}">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-danger">
+                                                    Eliminar
+                                                </a>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="row  hidden-md-up">
+                    <div class="col-md-6">
+                        {{ $games->links() }}
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-primary" href="{{ route('games.create') }}"> Añadir nuevo videojuego</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('games', 'GamesController@index')->name('games.index');
+Route::get('games/create', 'GamesController@create')->name('games.create');
+Route::post('games', 'GamesController@store')->name('games.store');
+Route::get('games/{id}/edit', 'GamesController@edit')->name('games.edit');
+Route::put('games/{id}', 'GamesController@update')->name('games.update');
+Route::delete('games/{id}', 'GamesController@destroy')->name('games.destroy');*/
+
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/home', 'home');
+Route::resource('games', 'GamesController');
